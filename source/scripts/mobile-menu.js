f0d2275ab7af83718ae94b3c5a3c199415cebdbd
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-02-07 18:28:47 +0100

    close mobile menu when click on bg

--- source/scripts/mobile-menu.js
+++ source/scripts/mobile-menu.js
@@ -2,25 +2,35 @@ const mobileMenu = () => {
 
   const mobileButton = document.querySelector('.header__menu-button');
   const navigation = document.querySelector('.nav');
+  const body = document.querySelector('.body');
   let menuStatus;
-
-  function openMobileMenu() {
-    navigation.classList.add('nav--open');
-    menuStatus = true;
-  }
-
-  function closeMobileMenu() {
-    navigation.classList.remove('nav--open');
-    menuStatus = false;
-  }
 
   const toogleMobileMenu = () => {
     menuStatus ? closeMobileMenu() : openMobileMenu();
   }
 
+  const onBackgroundClick = (evt) => {
+    if(evt.target.parentElement.tagName === 'HTML') {
+      closeMobileMenu();
+    }
+  }
+
+  function openMobileMenu() {
+    navigation.classList.add('nav--open');
+    body.classList.add('body--scrolloff');
+    menuStatus = true;
+
+    body.addEventListener('click', onBackgroundClick)
+  }
+
+  function closeMobileMenu() {
+    navigation.classList.remove('nav--open');
+    body.classList.remove('body--scrolloff');
+    body.removeEventListener('click', onBackgroundClick)
+    menuStatus = false;
+  }
+
   mobileButton.addEventListener('click', toogleMobileMenu);
-
-
 
 }
 
