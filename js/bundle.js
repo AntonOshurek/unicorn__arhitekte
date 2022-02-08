455ad962ee45b37de25697c077907b2c7dbed521
AntonOshurek <83346452+AntonOshurek@users.noreply.github.com>
2022-02-08 12:48:24 +0000

    Deploying to gh-pages from @ AntonOshurek/unicorn__arhitekte@218cde4fa70cf0cd35c16bc51749273fcb871379 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -36,6 +36,7 @@ const mobileMenu = () => {
   const mobileButton = document.querySelector('.header__menu-button');
   const mobileBtnIcon = document.querySelector('.ham');
   const navigation = document.querySelector('.nav');
+  const headerTitle = document.querySelector('.header__title');
   const body = document.querySelector('.body');
   let menuStatus;
 
@@ -53,6 +54,7 @@ const mobileMenu = () => {
     navigation.classList.add('nav--open');
     body.classList.add('body--menu-open');
     mobileBtnIcon.classList.add('active');
+    headerTitle.classList.add('header__title--menu');
     menuStatus = true;
     body.addEventListener('click', onBackgroundClick);
   }
@@ -61,8 +63,9 @@ const mobileMenu = () => {
     navigation.classList.remove('nav--open');
     body.classList.remove('body--menu-open');
     mobileBtnIcon.classList.remove('active');
-    body.removeEventListener('click', onBackgroundClick);
+    headerTitle.classList.remove('header__title--menu');
     menuStatus = false;
+    body.removeEventListener('click', onBackgroundClick);
   }
 
   mobileButton.addEventListener('click', toogleMobileMenu);
