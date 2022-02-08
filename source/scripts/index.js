9b74796b8d265c6afc99197ab2cb408495593626
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-02-08 12:32:50 +0100

    add all projects

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,14 +5,15 @@ import { showBigPicture } from './show-big-piture';
 import { saveProjectId } from './save-project-id';
 import { showCurrentProjects } from './show-current-project'
 
-window.addEventListener('DOMContentLoaded', () => {
+if(document.location.pathname === '/index.html' || document.location.pathname === '/unicorn__arhitekte/index.html') {
+  sliderFull();
+}
 
-  if(document.location.pathname === '/index.html' || document.location.pathname === '/unicorn__arhitekte/index.html') {
-    sliderFull();
-  }
+window.addEventListener('DOMContentLoaded', () => {
 
   if(document.location.pathname === '/projects.html' || document.location.pathname === '/unicorn__arhitekte/projects.html') {
     saveProjectId();
+
     if(window.innerWidth <= 900) {
       projectFocusedItem();
     }
