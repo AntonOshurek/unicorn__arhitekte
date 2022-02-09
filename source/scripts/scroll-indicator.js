6e7ff2670fa5d7b0ac9c5ecdab34771d11e24c8b
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-02-09 13:01:23 +0100

    start sript correaction

--- source/scripts/scroll-indicator.js
+++ source/scripts/scroll-indicator.js
@@ -1,14 +1,15 @@
 const srollIndicator = () => {
 
+  const srollIndicator = document.querySelector('.scroll-indicator');
   const bar = document.querySelector('.sroll-indicator__bar');
+
+  srollIndicator.classList.add('scroll-indicator--active');
 
   const myFunction = () => {
     const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     const scrolled = (winScroll / height) * 100;
     bar.style.height = scrolled + "%";
-
-    console.log(scrolled);
   };
 
   window.onscroll = function() {myFunction()};
