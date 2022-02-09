5f87d92d0688f165e95e44bab2b732dc0621558c
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-02-09 15:36:09 +0100

    close modal add button

--- source/scripts/show-big-piture.js
+++ source/scripts/show-big-piture.js
@@ -1,6 +1,7 @@
 const showBigPicture = () => {
 
   const modal = document.querySelector('.modal');
+  const closeModalButton = document.querySelector('.modal__close-btn');
   const modalImage = document.querySelector('.modal__picture');
   const gallery = document.querySelector('.project__gallery');
   const body = document.querySelector('.body');
@@ -14,20 +15,45 @@ const showBigPicture = () => {
 
   })
 
+  const onBackgroundClick = (evt) => {
+    if(evt.target.parentElement.tagName === 'HTML') {
+      closeModal();
+    }
+  }
+
+  const onModalEscKeydown = (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      closeModal();
+    }
+  };
+
+  // const windowBackBlock = () => {
+  //   window.onpopstate = (evt) => {
+  //     evt.preventDefault();
+  //     console.log('sdf ' + evt);
+  //     closeModal();
+  //     history.go(0);
+  //   };
+  // }
+
   function openModal(imageSourcce) {
     modal.classList.add('modal--open');
     modalImage.src = imageSourcce;
     body.classList.add('body--modal-open');
-    body.addEventListener('click', closeModal);
+    body.addEventListener('click', onBackgroundClick);
+    closeModalButton.addEventListener('click', closeModal);
+    document.addEventListener('keydown', onModalEscKeydown);
+    // windowBackBlock();
   };
 
-  function closeModal(evt) {
-    if(evt.target.parentElement.tagName === 'HTML') {
-      modal.classList.remove('modal--open');
-      modalImage.src = '';
-      body.classList.remove('body--modal-open');
-      body.removeEventListener('click', closeModal);
-    }
+  function closeModal() {
+    modal.classList.remove('modal--open');
+    modalImage.src = '';
+    body.classList.remove('body--modal-open');
+    body.removeEventListener('click', onBackgroundClick);
+    closeModalButton.removeEventListener('click', closeModal);
+    document.removeEventListener('keydown', onModalEscKeydown);
   };
 
 }
