6488cc8bb842d72cb6eb5e29571987fb241143fd
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-07-07 15:40:43 +0200

    delete unused js code

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,6 @@
 import { sliderFull } from './slider-full';
 import { mobileMenu } from './mobile-menu';
-import { showBigPicture } from './show-big-piture';
-import { saveProjectId } from './save-project-id';
-import { showCurrentProjects } from './show-current-project'
-import { srollIndicator } from './scroll-indicator';
-import { backToProjets } from './back-to-projects';
+// import { showBigPicture } from './show-big-piture';
 import { coloredItem } from './colored-item';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -14,18 +10,10 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   if(document.location.pathname === '/projects.html' || document.location.pathname === '/unicorn__arhitekte/projects.html') {
-    saveProjectId();
-    backToProjets();
 
     if(window.innerWidth <= 900) {
       coloredItem('.projects__card', 'data-item-id', 'projects__card--scrolled'); // itemName, dataAttribute , activeClass
-      srollIndicator();
     }
-  }
-
-  if(document.location.pathname === '/current-project.html' || document.location.pathname === '/unicorn__arhitekte/current-project.html') {
-    showCurrentProjects();
-    showBigPicture();
   }
 
   if(document.location.pathname === '/our-office.html' || document.location.pathname === '/unicorn__arhitekte/our-office.html') {
