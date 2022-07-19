135b049ba8a37618238f08d3f77b53d0d1ae3a6c
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-07-19 15:48:38 +0200

    refactoring project slider

--- source/scripts/project-slider.js
+++ source/scripts/project-slider.js
@@ -2,31 +2,42 @@
 let projectSlider = function () {
   const thumbnails = document.querySelectorAll('.project-slider__thumbnails-button');
   const allSlides = document.querySelectorAll('.project-slider__slide');
-  let slideName;
+
+  let currentSlide = thumbnails[0].getAttribute('data-slide-name'); // default value for first launch
 
   // for firs launch
-  selectSlideContent(thumbnails[0].getAttribute('data-slide-name'));
-  thumbnails[0].classList.add('project-slider__thumbnails-button--active');
+  setActiveSlide();
+  setActivetButton();
 
   thumbnails.forEach(item => {
-      item.addEventListener('click', selectSlide)
+    item.addEventListener('click', selectSlide);
   });
 
   function selectSlide() {
-    thumbnails.forEach(item => {
-          item.classList.remove('project-slider__thumbnails-button--active');
+    currentSlide = this.getAttribute('data-slide-name');
+    setActivetButton();
+    setActiveSlide();
+  };
+
+  function setActivetButton() {
+    thumbnails.forEach((button) => {
+      if(button.getAttribute('data-slide-name') === currentSlide) {
+        button.classList.add('project-slider__thumbnails-button--active');
+      } else {
+        button.classList.remove('project-slider__thumbnails-button--active');
+      }
     });
+  };
 
-    this.classList.add('project-slider__thumbnails-button--active');
-    slideName = this.getAttribute('data-slide-name');
-    selectSlideContent(slideName);
-  }
-
-  function selectSlideContent(slideName) {
+  function setActiveSlide() {
     allSlides.forEach(item => {
-          item.getAttribute('data-slide-name') === slideName ? item.classList.add('project-slider__slide--active') : item.classList.remove('project-slider__slide--active');
-      })
-  }
+      if(item.getAttribute('data-slide-name') === currentSlide) {
+        item.classList.add('project-slider__slide--active');
+      } else {
+        item.classList.remove('project-slider__slide--active');
+      }
+    });
+  };
 
 };
 
