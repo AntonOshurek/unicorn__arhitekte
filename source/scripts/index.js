efa5deaae99b3e430cd5d18c019b27a88efd2feb
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-07-28 17:03:19 +0200

    add suport all browsers in browser list >0%

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -8,14 +8,13 @@ import { TABLET_WIDHT, PROJECTS_ITEM_CLASS, PROJECTS_COLORED_ITEM_CLASS, TEAM_IT
 window.addEventListener('DOMContentLoaded', () => {
   mobileMenu(); // launch in all pages
 
-  if(document.location.pathname === '/projects.html' || document.location.pathname === '/unicorn__arhitekte/projects.html') {
-
+  if(document.location.pathname === '/projekty.html' || document.location.pathname === '/unicorn__arhitekte/projekty.html') {
     if(window.innerWidth <= TABLET_WIDHT) {
       coloredItem(PROJECTS_ITEM_CLASS, PROJECTS_COLORED_ITEM_CLASS); // itemName, activeClass
     }
   }
 
-  if(document.location.pathname === '/our-office.html' || document.location.pathname === '/unicorn__arhitekte/our-office.html') {
+  if(document.location.pathname === '/nasze-biuro.html' || document.location.pathname === '/unicorn__arhitekte/nasze-biuro.html') {
 
     if(window.innerWidth <= TABLET_WIDHT) {
       coloredItem( TEAM_ITEM_CLASS, TEAM_COLORED_ITEM_CLASS); // itemName, activeClass
