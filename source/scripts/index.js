dc8c41910d1d306f01f27e2e6f2ecdb401f64d70
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-08-03 16:00:13 +0200

    add new grid loyalts for projects page

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -9,11 +9,11 @@ import { TABLET_WIDHT, PROJECTS_ITEM_CLASS, PROJECTS_COLORED_ITEM_CLASS, TEAM_IT
 window.addEventListener('DOMContentLoaded', () => {
   mobileMenu(); // launch in all pages
 
-  if(document.location.pathname === '/projekty.html' || document.location.pathname === '/unicorn__arhitekte/projekty.html') {
-    if(window.innerWidth <= TABLET_WIDHT) {
-      coloredItem(PROJECTS_ITEM_CLASS, PROJECTS_COLORED_ITEM_CLASS); // itemName, activeClass
-    }
-  }
+  // if(document.location.pathname === '/projekty.html' || document.location.pathname === '/unicorn__arhitekte/projekty.html') {
+  //   if(window.innerWidth <= TABLET_WIDHT) {
+  //     coloredItem(PROJECTS_ITEM_CLASS, PROJECTS_COLORED_ITEM_CLASS); // itemName, activeClass
+  //   }
+  // }
 
   if(document.location.pathname === '/nasze-biuro.html' || document.location.pathname === '/unicorn__arhitekte/nasze-biuro.html') {
 
